49b65a8f6cbb024bb5f09eb6cebe9ff68dc6ff22
junbeompark <a01077348062@gmail.com>
2023-08-22 20:03:32 +0900

    create 로직 추가

--- crud_app/views.py
+++ crud_app/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Post 모델을 import
 from .models import Post
 
 # Create your views here.
 
-# Read관련 함수 
+# Read 관련 함수 
 def index(request): # 전체 게시물 출력
     posts = Post.objects.all()
     
@@ -23,3 +23,18 @@ def detail(request, id):    # 하나의 게시물 출력
     }
     
     return render (request, 'detail.html', context)
+
+# create 관련 함수
+def new(request):
+    return render(request, 'new.html')
+
+def create(request):
+    title = request.GET.get('title')
+    content = request.GET.get('content')
+    
+    post = Post()
+    post.title = title
+    post.content = content
+    post.save()
+    
+    return redirect(f'/posts/{post.id}/')
