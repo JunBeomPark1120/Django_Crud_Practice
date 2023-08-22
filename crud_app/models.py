99c44620bba53a67740b765d871ed3fbcc53414f
junbeompark <a01077348062@gmail.com>
2023-08-22 19:12:37 +0900

    모델 정의, 모델의 이름은 기본적으로 단수 형태

--- crud_app/models.py
+++ crud_app/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model):
-    title = models.charField(max_length=100)
+    title = models.CharField(max_length=100)
     content = models.TextField()
